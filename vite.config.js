1f47f6c621ae7e19d85aa9a4b7eb470ead803fce
SUMAN-MUDULI <sumanmuduli2004@gmail.com>
2025-10-04 17:56:12 +0530

    success

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/color-changer/", 
+  base: "/Color_Changer_Card/", 
   plugins: [react(),tailwindcss(),],
 })
